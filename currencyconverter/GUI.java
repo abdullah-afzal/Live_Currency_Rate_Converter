1f5d4775e8ec374acaaab9f8f92b5ec2b1edf0d5
Abdullah Afzal <mr.abdullahafzal@gmail.com>
2021-03-08 14:36:47 +0500

    Update GUI.java

--- currencyconverter/GUI.java
+++ currencyconverter/GUI.java
@@ -1,9 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package currencyconverter;
+
+/**
+ *
+ * @author Abdullah
+ */
 
 import java.awt.Color;
 import javax.swing.JButton;
