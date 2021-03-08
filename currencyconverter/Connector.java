64c394cb15b804adf63a91c0079910f0eb65305c
Abdullah Afzal <mr.abdullahafzal@gmail.com>
2021-03-08 14:34:53 +0500

    Update Connector.java

--- currencyconverter/Connector.java
+++ currencyconverter/Connector.java
@@ -1,9 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package currencyconverter;
+
+/**
+ *
+ * @author Abdullah
+ */
 
 import java.awt.Image;
 import java.io.BufferedReader;
